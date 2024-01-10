16f770352fbaf52df12e520b2004aa6d81185066
wnsdnn <a24369029@gmail.com>
2024-01-10 23:08:20 +0900

    색상 상수 지정하기

---
+++ project/actual/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    _App(),
+  );
+}
+
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
---
+++ 자료/flutter-lv2-rest-api-main/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:actual/common/component/custom_text_form_field.dart';
+import 'package:actual/common/provider/go_router.dart';
+import 'package:actual/common/view/splash_screen.dart';
+import 'package:actual/user/provider/auth_provider.dart';
+import 'package:actual/user/view/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: _App(),
+    ),
+  );
+}
+
+class _App extends ConsumerWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
