b69d457b32b70c93907cff78b3adc04ca3aa5122
wnsdnn <a24369029@gmail.com>
2024-01-12 13:32:35 +0900

    JSON 데이터 매핑하기

--- project/actual/lib/restaurant/view/restaurant_screen.dart
+++ project/actual/lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -32,6 +33,12 @@ class RestaurantScrenn extends StatelessWidget {
           child: FutureBuilder<List>(
             future: paginateRestaurant(),
             builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("에러가 발생하였습니다."),
+                );
+              }
+
               if (!snapshot.hasData) {
                 return Center(
                   child: CircularProgressIndicator(),
@@ -43,22 +50,33 @@ class RestaurantScrenn extends StatelessWidget {
               return ListView.separated(
                 itemBuilder: (context, index) {
                   final item = response[index]!;
-
-                  return RestaurantCard(
-                    // image: Image.asset(
-                    //   'asset/img/food/ddeok_bok_gi.jpg',
-                    //   fit: BoxFit.cover,
-                    // ),
-                    image: Image.network(
-                      'http://$ip/${item['thumbUrl']}',
-                      fit: BoxFit.cover,
-                    ),
+                  // parsed
+                  print(item['priceRange']);
+                  final pItem = RestaurantModel(
+                    id: item['id'],
                     name: item['name'],
+                    thumbUrl: 'http://$ip/${item['thumbUrl']}',
                     tags: List<String>.from(item['tags']),
+                    priceRange: RestaurantPriceRange.values.firstWhere(
+                      (element) => element.name == item['priceRange'],
+                    ),
+                    ratings: item['ratings'],
                     ratingsCount: item['ratingsCount'],
                     deliveryTime: item['deliveryTime'],
                     deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
+                  );
+
+                  return RestaurantCard(
+                    image: Image.network(
+                      pItem.thumbUrl,
+                      fit: BoxFit.cover,
+                    ),
+                    name: pItem.name,
+                    tags: pItem.tags,
+                    ratingsCount: pItem.ratingsCount,
+                    deliveryTime: pItem.deliveryTime,
+                    deliveryFee: pItem.deliveryFee,
+                    ratings: pItem.ratings,
                   );
                 },
                 separatorBuilder: (context, index) {
