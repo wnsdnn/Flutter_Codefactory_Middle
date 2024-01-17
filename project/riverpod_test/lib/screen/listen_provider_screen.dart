c15b7bae44efc86adeb140cfb4b5f25c149460e1
wnsdnn <a24369029@gmail.com>
2024-01-17 15:49:28 +0900

    Listen 함수 실습

---
+++ project/riverpod_test/lib/screen/listen_provider_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/listen_provider.dart';
+
+// State 앞에 Consumer 추가
+class ListenProviderScreen extends ConsumerStatefulWidget {
+  const ListenProviderScreen({super.key});
+
+  @override
+  ConsumerState<ListenProviderScreen> createState() =>
+      _ListenProviderScreenState();
+}
+
+class _ListenProviderScreenState extends ConsumerState<ListenProviderScreen>
+    with TickerProviderStateMixin {
+  late final TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(
+      length: 10,
+      vsync: this,
+      initialIndex: ref.read(listenProvider),
+      // initialIndex: ref.read(listenProvider.notifier).state,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ref 기본 제공
+    ref.listen<int>(listenProvider, (previous, next) {
+      if(previous != next) {
+        controller.animateTo(next);
+      }
+    });
+
+    return DefaultLayout(
+      title: 'ListenProviderScreen',
+      body: TabBarView(
+        controller: controller,
+        physics: NeverScrollableScrollPhysics(),
+        children: List.generate(
+          10,
+          (index) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(index.toString()),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(listenProvider.notifier).update((state) => state == 10 ? 10 : state + 1);
+                },
+                child: Text('다음'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(listenProvider.notifier).update((state) => state == 0 ? 0 : state - 1);
+                },
+                child: Text('뒤로'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
