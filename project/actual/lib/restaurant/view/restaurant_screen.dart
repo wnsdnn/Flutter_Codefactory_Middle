09b19bc1794f846546b85f58d2cf2110af3b60e8
wnsdnn <a24369029@gmail.com>
2024-01-19 14:25:04 +0900

    완성된 Pagination 로직 실행해보기

--- project/actual/lib/restaurant/view/restaurant_screen.dart
+++ project/actual/lib/restaurant/view/restaurant_screen.dart
@@ -5,24 +5,55 @@ import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScrenn extends ConsumerWidget {
+class RestaurantScrenn extends ConsumerStatefulWidget {
   const RestaurantScrenn({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScrenn> createState() => _RestaurantScrennState();
+}
+
+class _RestaurantScrennState extends ConsumerState<RestaurantScrenn> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가
+    // 최대 길이보다 조금 덜 되는 위치까지 왔다면
+    // 새로운 데이터를 추가요청
+    if (controller.offset > controller.position.maxScrollExtent - 200) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
-    if(data is CursorPaginationError) {
+    // 완전 처음 로딩일때
+    if (data is CursorPaginationLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    // 에러
+    if (data is CursorPaginationError) {
       return Center(
         child: Text(data.message),
       );
     }
 
-    if(data is CursorPaginationLoading) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
 
     final cp = data as CursorPagination;
 
@@ -31,6 +62,7 @@ class RestaurantScrenn extends ConsumerWidget {
         horizontal: 16.0,
       ),
       child: ListView.separated(
+        controller: controller,
         itemCount: cp.data.length,
         itemBuilder: (context, index) {
           final pItem = cp.data[index];
