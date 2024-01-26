5c8721c0ee9ca3f73cd422dc31d59d98b85787c6
wnsdnn <a24369029@gmail.com>
2024-01-26 10:35:25 +0900

    RatingCardImage 작업하기

--- project/actual/lib/rating/component/rating_card.dart
+++ project/actual/lib/rating/component/rating_card.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 class RatingCard extends StatelessWidget {
   // NetworkImage
@@ -37,7 +38,13 @@ class RatingCard extends StatelessWidget {
         _Body(
           content: content,
         ),
-        _Images(),
+        if(images.length > 0)
+          SizedBox(
+            height: 100.0,
+            child: _Images(
+              images: images,
+            ),
+          ),
       ],
     );
   }
@@ -114,10 +121,30 @@ class _Body extends StatelessWidget {
 }
 
 class _Images extends StatelessWidget {
-  const _Images({super.key});
+  final List<Image> images;
+
+  _Images({
+    super.key,
+    required this.images,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: images
+          .mapIndexed(
+            (index, e) => Padding(
+              padding: EdgeInsets.only(
+                right: index == images.length - 1 ? 0 : 16.0,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: e,
+              ),
+            ),
+          )
+          .toList(),
+    );
   }
 }
