621317fb5cd8f7058377f5976032dd3d41674c00
wnsdnn <a24369029@gmail.com>
2024-01-26 16:03:28 +0900

    RatingsPagination 렌더링하기

--- project/actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ project/actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/rating/component/rating_card.dart';
+import 'package:actual/rating/model/rating_model.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
@@ -60,18 +62,10 @@ class _RestaurantDetailScreenState
             renderProducts(
               products: state.products,
             ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0,),
-            sliver: SliverToBoxAdapter(
-              child: RatingCard(
-                avatarImage: AssetImage('asset/img/logo/codefactory_logo.png',),
-                images: [],
-                rating: 4,
-                email: 'jc@codefactory.ai',
-                content: '맛있습니다.',
-              ),
+          if (ratingsState is CursorPagination<RatingModel>)
+            renderRatings(
+              models: ratingsState.data,
             ),
-          ),
         ],
       ),
     );
@@ -126,6 +120,28 @@ class _RestaurantDetailScreenState
               child: ProductCard.fromModel(
                 model: model,
               ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderRatings({
+    required List<RatingModel> models,
+  }) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 16.0,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: models.length,
+              (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: RatingCard.fromModel(model: models[index]),
             );
           },
         ),
