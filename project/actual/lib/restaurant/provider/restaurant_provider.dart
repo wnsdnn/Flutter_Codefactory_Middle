48055e2d93c6e4e07dc0132c17ea088dc1acbaff
wnsdnn <a24369029@gmail.com>
2024-01-26 11:30:15 +0900

    RestaurantRatingProvider 초기작업

--- project/actual/lib/restaurant/provider/restaurant_provider.dart
+++ project/actual/lib/restaurant/provider/restaurant_provider.dart
@@ -167,6 +167,5 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
           )
           .toList(),
     );
-    //
   }
 }
--- project/actual/lib/restaurant/repository/restaurant_rating_repository.dart
+++ project/actual/lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,20 @@
+import 'package:actual/common/const/data.dart';
+import 'package:actual/common/dio/dio.dart';
 import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/common/model/pagination_params.dart';
 import 'package:actual/rating/model/rating_model.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
 
 part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingRepositoryProvider =
+    Provider.family<RestaurantRatingRepository, String>((ref, id) {
+  final dio = ref.watch(dioProvider);
+
+  return RestaurantRatingRepository(dio, baseUrl: 'http://$ip/restaurant/$id/rating');
+});
 
 // http://ip/restaurant/:rid/rating
 @RestApi()
