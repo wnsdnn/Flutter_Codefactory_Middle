4b7aa555c732eeea57d2180e3292a47a4ade113e
wnsdnn <a24369029@gmail.com>
2024-01-28 23:15:33 +0900

    Push 함수

--- project/go_router_v7_actual/lib/route/router.dart
+++ project/go_router_v7_actual/lib/route/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7_actual/screens/1_basic_scree.dart';
 import 'package:go_router_v7_actual/screens/2_named_screen.dart';
+import 'package:go_router_v7_actual/screens/3_push_screen.dart';
 import 'package:go_router_v7_actual/screens/root_screen.dart';
 
 // https://blog.codefactory.ai -> / -> path
@@ -19,6 +20,10 @@ final router = GoRouter(
           path: 'named',
           name: 'named_screen',
           builder: (context, state) => NamedScreen(),
+        ),
+        GoRoute(
+          path: 'push',
+          builder: (context, state) => PushScreen(),
         ),
       ],
     ),
