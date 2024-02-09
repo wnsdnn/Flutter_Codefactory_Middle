b9be9fa05d154293c62f9c43f319012c9c31cc64
wnsdnn <a24369029@gmail.com>
2024-02-09 22:20:29 +0900

    텍스트필드 디자인하기 및 파일 구조 변경

---
+++ project/actual2/lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,59 @@
+import 'package:actual2/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    super.key,
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // UnderlineInputBorder - TextFormField 기본값
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할때
+      obscureText: obscureText,
+      // 자동포커스
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20.0),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        // false - 배경색 없음
+        // true - 배경색 있음
+        filled: true,
+        // 모든 Input 상태의 기본 스타일 세팅
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- project/actual2/lib/common/const/colors.dart
+++ project/actual2/lib/common/const/colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const PRIMARY_COLOR = Color(0xFF22A45D);
 
 // 글자 색상
-const TEXT_BODY_COLOR = Color(0xFF868686);
+const BODY_TEXT_COLOR = Color(0xFF868686);
 
 // 텍스트필드 배경 색상
 const INPUT_BG_COLOR = Color(0xFFFBFBFB);
