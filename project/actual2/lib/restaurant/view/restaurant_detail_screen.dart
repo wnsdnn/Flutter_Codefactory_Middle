b49d37bca2037f34b812fb434fdb2919bb989251
wnsdnn <a24369029@gmail.com>
2024-02-12 23:17:51 +0900

    ProductCard 작업하기

--- project/actual2/lib/restaurant/view/restaurant_detail_screen.dart
+++ project/actual2/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual2/common/layout/default_layout.dart';
+import 'package:actual2/product/component/product_card.dart';
 import 'package:actual2/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +22,10 @@ class RestaurantDetailScreen extends StatelessWidget {
             ratings: 4.55,
             isDetail: true,
             detail: '맛있는 떡볶이',
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ProductCard(),
           ),
         ],
       ),
