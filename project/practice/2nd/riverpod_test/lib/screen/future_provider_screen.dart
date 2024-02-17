b92b8c404b77dfb91819490b0101764755c080b1
wnsdnn <a24369029@gmail.com>
2024-02-18 01:31:04 +0900

    FutureProvider 실습

---
+++ project/practice/2nd/riverpod_test/lib/screen/future_provider_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/future_provider.dart';
+
+class FutureProviderScreen extends ConsumerWidget {
+  const FutureProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue state = ref.watch(multiplesFutureProvider);
+
+    return DefaultLayout(
+      title: 'FutureProviderScreen',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          state.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (error, stackTrace) {
+              return Text(error.toString());
+            },
+            loading: () {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
