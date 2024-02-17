75e63f8a63ca66b62913e2760e6d9a5031e12f95
wnsdnn <a24369029@gmail.com>
2024-02-18 00:52:50 +0900

    StateProvider 실습

--- project/practice/2nd/riverpod_test/lib/screen/state_provider_screen.dart
+++ project/practice/2nd/riverpod_test/lib/screen/state_provider_screen.dart
@@ -31,6 +31,12 @@ class StateProviderScreen extends ConsumerWidget {
             ),
             ElevatedButton(
               onPressed: () {
+                ref.read(numberProvider.notifier).state = ref.read(numberProvider.notifier).state - 1;
+              },
+              child: Text('DOWN'),
+            ),
+            ElevatedButton(
+              onPressed: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (context) => _NextScreen(),
