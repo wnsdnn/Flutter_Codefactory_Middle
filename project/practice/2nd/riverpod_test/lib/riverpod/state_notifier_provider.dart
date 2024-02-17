b4ce7d44c9964f3fb2b17c0f1f8f00f311f3cff5
wnsdnn <a24369029@gmail.com>
2024-02-18 01:22:12 +0900

    StateNotifierProvider 실습

---
+++ project/practice/2nd/riverpod_test/lib/riverpod/state_notifier_provider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/model/shopping_item_model.dart';
+
+// StateNotifierProvider에는 재너릭을 꼭 써야하는데
+// <사용할 클래스, 해당클래스가 리턴하는 타입> 이렇게 2가지는 꼭 써줘야함
+final shppingListProvider =
+    StateNotifierProvider<ShppingListNotifier, List<ShppingItemModel>>(
+  (ref) => ShppingListNotifier(),
+);
+
+// StateNotifierProvider - 해당 클래스를 Provider로 만들어서 관리하는 것
+// StateNotifier - StateNotifierProvider안에 제공이 될 클래스가 상속하는 것
+class ShppingListNotifier extends StateNotifier<List<ShppingItemModel>> {
+  // super안에 처음에 어떤 상태로 값을 초기화할지 지정해줘야함
+  ShppingListNotifier()
+      : super(
+          [
+            ShppingItemModel(
+              name: '김치',
+              quantity: 3,
+              hasBought: false,
+              isSpicy: true,
+            ),
+            ShppingItemModel(
+              name: '라면',
+              quantity: 5,
+              hasBought: false,
+              isSpicy: true,
+            ),
+            ShppingItemModel(
+              name: '삼겹살',
+              quantity: 10,
+              hasBought: false,
+              isSpicy: false,
+            ),
+            ShppingItemModel(
+              name: '수박',
+              quantity: 2,
+              hasBought: false,
+              isSpicy: false,
+            ),
+            ShppingItemModel(
+              name: '카스테라',
+              quantity: 5,
+              hasBought: false,
+              isSpicy: false,
+            ),
+          ],
+        );
+
+  void toggleHasBought({
+    required String name,
+  }) {
+    // state(클래스에서 자동으로 제공됨)
+    // ref.read([provider]).state와 같음
+    state = state
+        .map((e) => e.name == name
+            ? ShppingItemModel(
+                name: e.name,
+                quantity: e.quantity,
+                hasBought: !e.hasBought,
+                isSpicy: e.isSpicy,
+              )
+            : e)
+        .toList();
+  }
+}
---
+++ project/practice/2nd/riverpod_test/lib/screen/state_notifier_provider_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/model/shopping_item_model.dart';
+import 'package:riverpod_test/riverpod/state_notifier_provider.dart';
+
+class StateNotifierProviderScreen extends ConsumerWidget {
+  const StateNotifierProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<ShppingItemModel> state = ref.watch(shppingListProvider);
+
+    return DefaultLayout(
+      title: 'StateNotifierProvider',
+      body: ListView(
+        children: state
+            .map(
+              (e) => CheckboxListTile(
+                title: Text(e.name),
+                value: e.hasBought,
+                onChanged: (value) {
+                  ref.read(shppingListProvider.notifier).toggleHasBought(
+                        name: e.name,
+                      );
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
