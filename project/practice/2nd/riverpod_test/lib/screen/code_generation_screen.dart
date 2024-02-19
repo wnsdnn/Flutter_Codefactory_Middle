26118a0b3448414ace0090cd767931c38339480e
wnsdnn <a24369029@gmail.com>
2024-02-20 00:14:38 +0900

    Riverpod v2 Consumer 위젯 사용하기

--- project/practice/2nd/riverpod_test/lib/screen/code_generation_screen.dart
+++ project/practice/2nd/riverpod_test/lib/screen/code_generation_screen.dart
@@ -8,6 +8,8 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+
     // 불러올 Provider는 개발자가 선언한 함수명이 아닌 .g.dart 파일에 생성된 Provider를 가져와야 한다.
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
@@ -16,7 +18,6 @@ class CodeGenerationScreen extends ConsumerWidget {
       number1: 10,
       number2: 20,
     ));
-    final state5 = ref.watch(gStateNotifierProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -39,7 +40,25 @@ class CodeGenerationScreen extends ConsumerWidget {
             ),
           ),
           Text('State4: $state4'),
-          Text('State5: $state5'),
+          // Consumer - build 함수 안에다가 부분적으로 반환해주고 싶은값을 리턴해주면 됨
+          Consumer(
+            builder: (context, ref, child) {
+              print('builder build');
+              final state5 = ref.watch(gStateNotifierProvider);
+
+              return Row(
+                children: [
+                  Text('State5: $state5'),
+                  if(child != null) child,
+                ],
+              );
+            },
+            // child의 값은 딱 1번만 렌더링 된다
+            // build가 다시 실행되도 렌더링 안함
+            // 렌더링할때 비용이 많이 드는 위젯들을 보통 집어넣음
+            child: Text('Hello'),
+          ),
+          // _StateFiveWidget(),
           Row(
             children: [
               ElevatedButton(
@@ -67,5 +86,16 @@ class CodeGenerationScreen extends ConsumerWidget {
         ],
       ),
     );
+  }
+}
+
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+
+    return Text('State5: $state5');
   }
 }
