88b0853401ae6b421706d54132750f218c478814
wnsdnn <a24369029@gmail.com>
2024-02-24 22:02:26 +0900

    RatingModel 작업하기

--- project/actual2/lib/common/utils/data_utils.dart
+++ project/actual2/lib/common/utils/data_utils.dart
@@ -1,7 +1,11 @@
 import 'package:actual2/common/const/data.dart';
 
 class DataUtils {
-  static pathToUrl(String path) {
+  static String pathToUrl(String path) {
     return 'http://$ip/$path';
+  }
+
+  static List<String> listPathsToUrls(List<String> paths) {
+    return paths.map((e) => pathToUrl(e)).toList();
   }
 }
