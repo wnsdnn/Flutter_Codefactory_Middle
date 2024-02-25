0f8c5a0d9c0440b54fc47fe6dd915fd4d08373bb
wnsdnn <a24369029@gmail.com>
2024-02-25 22:51:35 +0900

    RestaurantRatingProvider 초기작업

--- project/actual2/lib/restaurant/repository/restaurant_rating_repository.dart
+++ project/actual2/lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,22 @@
+import 'package:actual2/common/const/data.dart';
+import 'package:actual2/common/dio/dio.dart';
 import 'package:actual2/common/model/cursor_pagination_model.dart';
 import 'package:actual2/common/model/pagination_params.dart';
 import 'package:actual2/rating/model/rating_model.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
 
 part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingRepositoryProvider =
+    Provider.family<RestaurantRatingRepository, String>(
+  (ref, id) {
+    final dio = ref.watch(dioProvider);
+
+    return RestaurantRatingRepository(dio, baseUrl: 'http://$ip/restaurant/$id/rating');
+  },
+);
 
 // http://$ip/restaurant/:rid/rating
 @RestApi()
