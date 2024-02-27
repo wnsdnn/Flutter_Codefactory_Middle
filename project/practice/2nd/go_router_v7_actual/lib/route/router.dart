a0fa2f0f227a7d79833a54b78d2aa70142a973ac
wnsdnn <a24369029@gmail.com>
2024-02-27 22:11:31 +0900

    Go 함수

--- project/practice/2nd/go_router_v7_actual/lib/route/router.dart
+++ project/practice/2nd/go_router_v7_actual/lib/route/router.dart
@@ -1,13 +1,22 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_v7_actual/screen/1_basic_screen.dart';
 import 'package:go_router_v7_actual/screen/root_screen.dart';
 
 // http://blog.codefactory.ai -> /
 // https://blog.codefactory.ai/flutter -> /flutter
+// / -> home
+// /basic -> basic screen
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => RootScreen(),
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) => BasicScreen(),
+        ),
+      ],
     ),
   ],
 );
