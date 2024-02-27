770cc4298c58a90ea40996fe99e89601b0eabff4
wnsdnn <a24369029@gmail.com>
2024-02-27 22:29:03 +0900

    Push 함수

--- project/practice/2nd/go_router_v7_actual/lib/route/router.dart
+++ project/practice/2nd/go_router_v7_actual/lib/route/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7_actual/screen/1_basic_screen.dart';
 import 'package:go_router_v7_actual/screen/2_named_screen.dart';
+import 'package:go_router_v7_actual/screen/3_push_screen.dart';
 import 'package:go_router_v7_actual/screen/root_screen.dart';
 
 // http://blog.codefactory.ai -> /
@@ -22,6 +23,10 @@ final router = GoRouter(
           path: 'named',
           name: 'named_screen',
           builder: (context, state) => NamedScreen(),
+        ),
+        GoRoute(
+          path: 'push',
+          builder: (context, state) => PushScreen(),
         ),
       ],
     ),
--- project/practice/2nd/go_router_v7_actual/lib/screen/3_push_screen.dart
+++ project/practice/2nd/go_router_v7_actual/lib/screen/3_push_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7_actual/layout/default_layout.dart';
 
-class RootScreen extends StatelessWidget {
-  const RootScreen({super.key});
+class PushScreen extends StatelessWidget {
+  const PushScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +12,15 @@ class RootScreen extends StatelessWidget {
         children: [
           ElevatedButton(
             onPressed: () {
+              context.push('/basic');
+            },
+            child: Text('Push Basic'),
+          ),
+          ElevatedButton(
+            onPressed: () {
               context.go('/basic');
             },
             child: Text('Go Basic'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.goNamed('named_screen');
-            },
-            child: Text('Go Named'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/push');
-            },
-            child: Text('Go Push'),
           ),
         ],
       ),
