481d5c42efc2fd5e27c2e50de69302d47343e548
wnsdnn <a24369029@gmail.com>
2024-02-28 23:46:01 +0900

    AuthProvider 작업하기

--- project/actual2/lib/user/provider/user_me_provider.dart
+++ project/actual2/lib/user/provider/user_me_provider.dart
@@ -1,9 +1,25 @@
 import 'package:actual2/common/const/data.dart';
+import 'package:actual2/common/secure_storage/secure_storage.dart';
 import 'package:actual2/user/model/user_model.dart';
 import 'package:actual2/user/repository/auth_repository.dart';
 import 'package:actual2/user/repository/user_me_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final userMeProvider =
+    StateNotifierProvider<UserMeStateNotifier, UserModelBase?>(
+  (ref) {
+    final authRepository = ref.watch(authRepositoryProvider);
+    final userMeRepository = ref.watch(userMeRepositoryProvider);
+    final storage = ref.watch(secureStorageProvider);
+    
+    return UserMeStateNotifier(
+      authRepository: authRepository,
+      repository: userMeRepository,
+      storage: storage,
+    );
+  },
+);
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
   final AuthRepository authRepository;
@@ -54,7 +70,7 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
       state = userResp;
 
       return userResp;
-    } catch(e) {
+    } catch (e) {
       state = UserModelError(message: '로그인에 실패했습니다.');
 
       return Future.value(state);
--- project/actual2/lib/user/repository/auth_repository.dart
+++ project/actual2/lib/user/repository/auth_repository.dart
@@ -1,17 +1,25 @@
 import 'package:actual2/common/const/data.dart';
+import 'package:actual2/common/dio/dio.dart';
 import 'package:actual2/common/model/login_response.dart';
 import 'package:actual2/common/model/token_response.dart';
 import 'package:actual2/common/utils/data_utils.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return AuthRepository(dio: dio, baseUrl: 'http://$ip/auth');
+});
 
 // http://$ip/auth
 class AuthRepository {
-  final String baseUrl;
   final Dio dio;
+  final String baseUrl;
 
   AuthRepository({
-    required this.baseUrl,
     required this.dio,
+    required this.baseUrl,
   });
 
   // 로그인 해주는 함수
