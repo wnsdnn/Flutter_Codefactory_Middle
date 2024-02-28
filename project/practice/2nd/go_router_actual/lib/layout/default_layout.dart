13d326b3eba1b15f9567e05c9c1dab8b0a476882
wnsdnn <a24369029@gmail.com>
2024-02-28 23:51:12 +0900

    프로젝트 세팅하기

---
+++ project/practice/2nd/go_router_actual/lib/layout/default_layout.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final Widget body;
+
+  const DefaultLayout({
+    super.key,
+    required this.body,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: body,
+      ),
+    );
+  }
+}
---
+++ project/practice/2nd/go_router_actual/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:go_router_actual/screen/home_screen.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
