f75026c297012805009a4a8cb7ce76add208aeb2
wnsdnn <a24369029@gmail.com>
2024-03-01 01:17:32 +0900

    Go 함수

--- project/practice/2nd/go_router_actual/lib/layout/default_layout.dart
+++ project/practice/2nd/go_router_actual/lib/layout/default_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget body;
@@ -10,9 +11,11 @@ class DefaultLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final location = GoRouterState.of(context).location;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('test'),
+        title: Text(location.toString()),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
--- project/practice/2nd/go_router_actual/lib/main.dart
+++ project/practice/2nd/go_router_actual/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:go_router_actual/screen/1_screen.dart';
 import 'package:go_router_actual/screen/home_screen.dart';
 
 void main() {
@@ -15,6 +16,12 @@ class _App extends StatelessWidget {
           GoRoute(
             path: '/',
             builder: (context, state) => HomeScreen(),
+            routes: [
+              GoRoute(
+                path: 'one',
+                builder: (context, state) => OneScreen(),
+              ),
+            ],
           ),
         ],
       );
@@ -23,15 +30,16 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       // 라우터 정보를 리턴
-      routeInformationProvider: _router.routeInformationProvider,
+      // routeInformationProvider: _router.routeInformationProvider,
       // URI String을 상태 및 Go Router에서 사용할 수 있는 형태로
       // 변환해주는 함수
-      routeInformationParser: _router.routeInformationParser,
+      // routeInformationParser: _router.routeInformationParser,
       // 위에서 변경된 값으로
       // 실제 어떤 라우트를 보여줄지
       // 정하는 함수
-      routerDelegate: _router.routerDelegate,
-      // home: HomeScreen(),
+      // routerDelegate: _router.routerDelegate,
+
+      routerConfig: _router,
     );
   }
 }
