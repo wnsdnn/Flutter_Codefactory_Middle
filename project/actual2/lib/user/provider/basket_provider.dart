cb89d4f587e989c12ae586334014940e934e3980
wnsdnn <a24369029@gmail.com>
2024-03-02 00:32:08 +0900

    Optimistic Response

--- project/actual2/lib/user/provider/basket_provider.dart
+++ project/actual2/lib/user/provider/basket_provider.dart
@@ -1,21 +1,49 @@
 import 'package:actual2/product/model/product_model.dart';
 import 'package:actual2/user/model/basket_item_model.dart';
+import 'package:actual2/user/model/patch_basket_body.dart';
+import 'package:actual2/user/repository/user_me_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:collection/collection.dart';
 
 final basketProvider =
     StateNotifierProvider<BasketProvider, List<BasketItemModel>>(
   (ref) {
-    return BasketProvider();
+    final repository = ref.watch(userMeRepositoryProvider);
+
+    return BasketProvider(
+      repository: repository,
+    );
   },
 );
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
-  BasketProvider() : super([]);
+  final UserMeRepository repository;
+
+  BasketProvider({
+    required this.repository,
+  }) : super([]);
+
+  Future<void> patchBasket() async {
+    await repository.patchBasket(
+      body: PatchBasketBody(
+        basket: state.map(
+          (e) => PatchBasketBodyBasket(
+            productId: e.product.id,
+            count: e.count,
+          ),
+        ).toList(),
+      ),
+    );
+  }
 
   Future<void> addToBasket({
     required ProductModel product,
   }) async {
+    // 요청을 먼저 보내고
+    // 응답이 오면
+    // 캐시를 업데이트 했다.
+    // await Future.delayed(Duration(milliseconds: 500));
+
     // 1) 아직 장바구니에 해당되는 상품이 없다면
     // 장바구니에 상품을 추가한다.
 
@@ -42,6 +70,14 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
         ),
       ];
     }
+
+    // 요청을 성공할거라고 예측하고
+    // 캐시를 먼저 업데이트를 하고
+    // 요청을 보낸다
+    // Optimistic Reponse (긍정적 응답)
+    // 응답이 성공할거라고 가정하고 상태를 먼저 업데이트함
+
+    await patchBasket();
   }
 
   Future<void> removeFromBasket({
@@ -82,5 +118,7 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
           )
           .toList();
     }
+
+    await patchBasket();
   }
 }
