a143b08ada605bc33d6103aadfadf8eb16449957
wnsdnn <a24369029@gmail.com>
2024-03-02 19:19:42 +0900

    OrderModel 생성하기

--- project/actual2/lib/common/utils/data_utils.dart
+++ project/actual2/lib/common/utils/data_utils.dart
@@ -3,6 +3,10 @@ import 'dart:convert';
 import 'package:actual2/common/const/data.dart';
 
 class DataUtils {
+  static DateTime stringToDateTime(String value) {
+    return DateTime.parse(value);
+  }
+
   static String pathToUrl(String path) {
     return 'http://$ip/$path';
   }
